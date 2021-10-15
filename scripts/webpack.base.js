157fe95b6e3f62a5aa8f2943610507af508b4464
yanmei <yanmei@gmail.com>
2021-10-15 16:38:00 +0800

    提交基础

---
+++ scripts/webpack.base.js
@@ -0,0 +1,73 @@
+// webpack.base.js
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  // 入口文件，这里之后会着重强调
+  entry: {
+    main: path.resolve(__dirname, '../src/packages/home/index.jsx'),
+  },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, '../src'),
+      '@packages': path.resolve(__dirname, '../src/packages'),
+      '@containers': path.resolve(__dirname, '../src/containers'),
+    },
+    mainFiles: ['index', 'main'],
+    extensions: ['.ts', '.tsx', '.scss', 'json', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              keepQuery: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+    // 生成html名称为index.html
+    // 生成使用的模板为public/index.html
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../public/index.html'),
+    }),
+  ],
+};
