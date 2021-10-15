0b2010011be49c4b129ba51904beb42bb973bc68
yanmei <yanmei@gmail.com>
2021-10-15 17:17:44 +0800

    1

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  "src/**/*.{js,ts,vue}": [
-    "eslint --fix --ext .js,.ts,.vue",
-    "prettier --write",
-  ],
-};
+  'src/**/*.{js,ts}': ['eslint --fix --ext .js,.ts', 'prettier --write'],
+}
